cc1a688a5f56d7a302b17e10cf66158c093e8796
yli0607x <github email address>
2018-11-15 09:20:46 -0500

    something

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,55 +1,61 @@
 class RestaurantsController < ApplicationController
-  before_action :find_restaurant, to: [:show, :destroy, :edit, :update]
-  skip_before_action :authorized, only: [:index, :show]
+    before_action :find_restaurant, to: %i[show destroy edit update]
+    skip_before_action :authorized, only: %i[index show]
 
-  def index
-    @restaurants = Restaurant.all
-    if params[:search]
-      @restaurants = Restaurant.search(params[:search])
-    else
-      @restaurants = Restaurant.all
+    def index
+        @restaurants = Restaurant.all
+        @restaurants = if params[:search]
+                           Restaurant.search(params[:search])
+                       else
+                           Restaurant.all
+                       end
     end
-  end
 
-  def show
-    @review = Review.find_by(id:params[:id])
-    flash[:restaurant_id] = @restaurant.id
-  end
-
-  def new
-    @restaurant = Restaurant.new
-  end
-
-  def create
-    @restaurant = Restaurant.create(restaurant_params)
-    if @restaurant.valid?
-      redirect_to restaurant_path(@restaurant)
-    else
-      flash[:errors] = @restaurant.errors.full_messages
-      redirect_to new_restaurant_path
+    def show
+        @review = Review.find_by(id: params[:id])
+        flash[:restaurant_id] = @restaurant.id
     end
-  end
 
-  def edit
-  end
+    def new
+        @restaurant = Restaurant.new
+    end
 
-  def update
-    @restaurant.update(restaurant_params)
-    redirect_to restaurant_path(@restaurant)
-  end
+    def create
+        @restaurant = Restaurant.create(restaurant_params)
+        if @restaurant.valid?
+            redirect_to restaurant_path(@restaurant)
+        else
+            flash[:errors] = @restaurant.errors.full_messages
+            redirect_to new_restaurant_path
+        end
+    end
 
-  def destroy
-    @restaurant.destroy
-    redirect_to restaurants_path
-  end
+    def edit; end
 
+    def update
+        if restaurant_params[:restaurant_photo]
+            restaurant_params[:restaurant_photo] = restaurant_params[:restaurant_photo].original_filename
+        end
+        @restaurant.update_attributes(restaurant_params)
+        redirect_to restaurant_path(@restaurant)
+    end
 
-  private
-  def restaurant_params
-    params.require(:restaurant).permit(:name, :address, :phone, :credit_card, :delivery, :longitude, :latitude, :search, :restaurant_photo)
-  end
+    def destroy
+        @restaurant.destroy
+        redirect_to restaurants_path
+    end
 
-  def find_restaurant
-    @restaurant = Restaurant.find_by(id: params[:id])
-  end
+    private
+
+    def restaurant_params
+        params.require(:restaurant).permit(:name, :address, :phone, :credit_card, :delivery, :longitude, :latitude, :search, :restaurant_photo)
+    end
+
+    def find_restaurant
+        @restaurant = Restaurant.find_by(id: params[:id])
+    end
+
+    def update_rest_photo
+        update_photo = params.require(:restaurant).permit(:restaurant_photo).original_filename
+    end
 end
