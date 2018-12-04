2499e9f73f25c48e893718fda2b58d2551d43b99
yli0607x <github email address>
2018-12-04 15:33:46 -0500

    final update

--- db/seeds.rb
+++ db/seeds.rb
@@ -43,7 +43,7 @@ Restaurant.create(
 )
 
 Restaurant.create(
-    name: 'Carbone',
+    name: 'Uchu',
     address: '217 Eldridge St, New York, NY 10002',
     phone: '(212) 203-7634',
     delivery: true,
@@ -111,14 +111,44 @@ Photo.create(restaurant_id: 5, url: 'http://i67.tinypic.com/30rwwtd.jpg')
 Photo.create(restaurant_id: 5, url: 'http://i66.tinypic.com/2j3imoi.jpg')
 Photo.create(restaurant_id: 5, url: 'http://i67.tinypic.com/2gxmrtt.jpg')
 
-Photo.create(restaurant_id: 1, url: "https://image.ibb.co/iApBN0/IMG-3112.jpg")
+Photo.create(restaurant_id: 1, url: "https://cdn.vox-cdn.com/thumbor/7r3Xld1VO-WS6HS8_UhydAbL428=/259x0:4450x3143/1200x800/filters:focal(259x0:4450x3143)/cdn.vox-cdn.com/uploads/chorus_image/image/48825293/24392094161_cd52a90e32_o.0.0.jpg")
 Photo.create(restaurant_id: 1, url: "https://image.ibb.co/kBoSvL/IMG-3077.jpg")
 Photo.create(restaurant_id: 1, url: "https://image.ibb.co/nteZaL/IMG-3082.jpg")
-Photo.create(restaurant_id: 1, url: "https://image.ibb.co/heTuaL/IMG-3087.jpg")
-Photo.create(restaurant_id: 1, url: "https://image.ibb.co/fvJuaL/IMG-3090.jpg")
-Photo.create(restaurant_id: 1, url: "https://image.ibb.co/dO1rN0/IMG-3097.jpg")
-Photo.create(restaurant_id: 1, url: "https://image.ibb.co/cZAJ20/IMG-3102.jpg")
-Photo.create(restaurant_id: 1, url: "https://image.ibb.co/iPhWN0/IMG-3109.jpg")
+Photo.create(restaurant_id: 1, url: "http://www.madeinjoo.com/wp-content/uploads/chefstable_brooklynfare_4.jpg")
+Photo.create(restaurant_id: 1, url: "http://www.madeinjoo.com/wp-content/uploads/chefstable_brooklynfare_10.jpg")
+Photo.create(restaurant_id: 1, url: "https://s3-media2.fl.yelpcdn.com/bphoto/iBjqfMs6mpMh9FqIT-oewQ/o.jpg")
+Photo.create(restaurant_id: 1, url: "http://www.madeinjoo.com/wp-content/uploads/chefstable_brooklynfare_23.jpg")
+Photo.create(restaurant_id: 1, url: "http://www.madeinjoo.com/wp-content/uploads/chefstable_brooklynfare_6.jpg")
+
+Photo.create(restaurant_id: 4, url: "http://www.madeinjoo.com/wp-content/uploads/uchu-9.jpg")
+Photo.create(restaurant_id: 4, url: "http://www.madeinjoo.com/wp-content/uploads/uchu-13.jpg")
+Photo.create(restaurant_id: 4, url: "http://www.madeinjoo.com/wp-content/uploads/uchu-15.jpg")
+Photo.create(restaurant_id: 4, url: "http://www.madeinjoo.com/wp-content/uploads/uchu-8.jpg")
+
+Photo.create(restaurant_id: 3, url: "https://cdn-images-1.medium.com/max/1600/1*AB9hwNhmWlBBdU6UVv9xSw.png")
+Photo.create(restaurant_id: 3, url: "https://assets3.thrillist.com/v1/image/2576170/size/tmg-article_default_mobile.jpg")
+Photo.create(restaurant_id: 3, url: "https://carbonenewyork.com/assets/images/slides/beef-carpaccio-3.jpg")
+Photo.create(restaurant_id: 3, url: "https://carbonenewyork.com/assets/images/slides/uni-6.jpg")
+
+Photo.create(restaurant_id: 8, url: "https://image.ibb.co/ngBm5L/lacontena-4.jpg")
+Photo.create(restaurant_id: 8, url: "https://image.ibb.co/doB8Kf/lacontena-5.jpg")
+Photo.create(restaurant_id: 8, url: "https://image.ibb.co/cTwKQL/lacontenta-2.jpg")
+Photo.create(restaurant_id: 8, url: "https://image.ibb.co/dVRKQL/lacontea-1.jpg")
+
+Photo.create(restaurant_id: 2, url: "https://media2.trover.com/T/555cf85bae8d8a5ad0006cec/fixedw_large_2x.jpg")
+Photo.create(restaurant_id: 2, url: "http://www.johnnyprimesteaks.com/wp-content/uploads/2017/03/DSC03973-1024x684.jpg")
+Photo.create(restaurant_id: 2, url: "https://media-cdn.tripadvisor.com/media/photo-s/07/bd/d1/52/peter-luger-steak-house.jpg")
+Photo.create(restaurant_id: 2, url: "http://tastetheworld.vista03.net/wp/wp-content/uploads/2017/04/stakehouse_stake.jpg")
+
+Photo.create(restaurant_id: 6, url: "http://www.turntablenyc.com/wp-content/uploads/2014/07/turntable-food-03.jpg")
+Photo.create(restaurant_id: 6, url: "https://www.turntablenyc.com/wp-content/uploads/2017/07/turntablechickenjazz33st.png")
+Photo.create(restaurant_id: 6, url: "https://s3-media3.fl.yelpcdn.com/bphoto/UIs7MTEVI2CblymwkC_TWg/348s.jpg")
+Photo.create(restaurant_id: 6, url: "http://www.hungryones.com/wp-content/uploads/2016/10/turntable.1905.jpg")
+
+Photo.create(restaurant_id: 7, url: "https://s3-media2.fl.yelpcdn.com/bphoto/CpVI2XJLrvg09Fm83bfPog/o.jpg")
+Photo.create(restaurant_id: 7, url: "https://www.nycgo.com/images/venues/620/obao-phil-kline-20__x_large.jpg")
+Photo.create(restaurant_id: 7, url: "https://media.timeout.com/images/100335651/630/472/image.jpg")
+Photo.create(restaurant_id: 7, url: "https://resizer.otstatic.com/v2/photos/huge/25649681.jpg")
 
 #
 # Photo.create(restaurant_id: 1, user_id: 1)
